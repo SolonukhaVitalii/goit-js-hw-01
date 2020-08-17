b779f11bea6cbf2a4c32725467e8c45bdbfed368
Vitalii <v.solonuha@gmail.com>
2020-08-17 16:56:00 +0300

    work 1

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const name = '«Генератор защитного поля»';
+let price = 1000; 
+const firstMessage = `Выбран «Генератор защитного поля», цена за штуку 1000 кредитов`; 
+    price = 2000;
+
+console.log(firstMessage);
+
+const secondMessage = `Выбран «Генератор защитного поля», цена за штуку 2000 кредитов`;
+
+console.log(secondMessage);
---
+++ js/task-2.js
@@ -0,0 +1,6 @@
+const invoice = 100; 
+const stock = 100;
+
+const message = invoice <= stock ? "Заказ оформлен, с вами свяжется менеджер" : "На складе недостаточно товаров!";// Write code under this line
+
+console.log(message);
